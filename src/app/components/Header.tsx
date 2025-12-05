cf4220e1943f6681eb6d596e69ba5b4028cdd402
GEUNNN <geunhwa.lee.01@gmail.com>
2025-12-05 10:25:22 +0900

    feat: add elements to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-
 const Header = () => {
   return (
-    <div>
-      <div>hi I am</div>
-      Header
-    </div>
+    <header className="sticky top-0 flex items-center justify-between p-4 shadow-md z-50">
+      <h1>Geunhwa Lee</h1>
+      <nav>
+        <ul className="flex flex-row">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div>KO/EN</div>
+    </header>
   );
 };
 
